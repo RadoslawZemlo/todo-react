d0b2b844d4df2e58471aebe4d3defbd254d99cbe
RadoslawZemlo <radoslawzemlo1@gmail.com>
2021-12-05 17:22:13 +0100

    Add TodoList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo App</h1>
+      <header>
+        <h1>Todo App</h1>
+      </header>
       <TodoForm />
+      <TodoList />
     </div>
   );
 }
